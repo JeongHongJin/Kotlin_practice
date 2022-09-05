7be56d1116024593c99114c96421f428cff9b103
unknown <mistake9518@gmail.com>
2022-09-05 12:10:47 +0900

    feat: Practice8.kt

---
+++ kotlin_practice/src/main/kotlin/Practice8.kt
@@ -0,0 +1,41 @@
+fun main() {
+    repeat("Hello World", useNewLine = false)
+    printNameAndGender(gender = "FEMALE", name = "정홍진")
+    printAll("A", "B", "C")
+    val array = arrayOf("A", "B", "C")
+    printAll(*array)
+}
+
+fun max(a: Int, b: Int): Int =
+    if (a > b) {
+        a
+    } else {
+        b
+    }
+
+fun max2(a: Int, b: Int) = if (a > b) a else b
+
+fun repeat(
+    str: String,
+    num: Int = 3,
+    useNewLine: Boolean = true
+) {
+    for (i in 1..num) {
+        if (useNewLine) {
+            println(str)
+        } else {
+            print(str)
+        }
+    }
+}
+
+fun printNameAndGender(name: String, gender: String) {
+    println(name)
+    println(gender)
+}
+
+fun printAll(vararg strings: String) {
+    for (str in strings) {
+        println(str)
+    }
+}
