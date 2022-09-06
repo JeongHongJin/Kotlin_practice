15bc479fe3466532e061ee763c5689f5fedfeb36
unknown <mistake9518@gmail.com>
2022-09-06 12:43:26 +0900

    feat: Practice10.kt

---
+++ kotlin_practice/src/main/kotlin/Practice10.kt
@@ -0,0 +1,21 @@
+//Accessing non-final property number in constructor
+fun main(){
+    Derived(300)
+}
+
+open class Base(
+    open val number: Int = 100
+){
+    init {
+        println("Base Class")
+        println(number)
+    }
+}
+
+class Derived(
+    override val number: Int
+) : Base(number){
+    init {
+        println("Derived Class")
+    }
+}
